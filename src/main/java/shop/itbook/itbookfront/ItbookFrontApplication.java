8a1949c7624becc8e5a45b2405c687f3e73b4bc0
yihoney <109622@naver.com>
2023-01-03 11:19:29 +0900

    first commit

---
+++ src/main/java/shop/itbook/itbookfront/ItbookFrontApplication.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbookFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItbookFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/itbook/itbookfront/ItbookFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbookFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
