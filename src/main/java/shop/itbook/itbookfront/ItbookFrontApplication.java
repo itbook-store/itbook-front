dd02a787672b6e6d42cae5f2742147d765a1d125
yihoney <109622@naver.com>
2023-01-03 11:22:57 +0900

    second commit

--- src/main/java/shop/itbook/itbookfront/ItbookFrontApplication.java
+++ src/main/java/shop/itbook/itbookfront/ItbookFrontApplication.java
@@ -8,6 +8,7 @@ public class ItbookFrontApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ItbookFrontApplication.class, args);
+        System.out.println("hi");
     }
 
 }
