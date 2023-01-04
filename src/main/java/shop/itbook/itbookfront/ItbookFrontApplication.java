921627305d34967beafe3c214c6be33b74e904c4
yihoney <109622@naver.com>
2023-01-04 09:33:09 +0900

    [chore] git ignore 파일 추가

--- src/main/java/shop/itbook/itbookfront/ItbookFrontApplication.java
+++ src/main/java/shop/itbook/itbookfront/ItbookFrontApplication.java
@@ -3,9 +3,17 @@ package shop.itbook.itbookfront;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ItbookFrontApplication {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @author yihoney *
+     * fgfgfgff
+     */
     public static void main(String[] args) {
 
 
