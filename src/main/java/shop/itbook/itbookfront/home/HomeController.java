dc641f2a0f6a8bea5c85a858f3b3bbb5a517e366
jun1718 <gbeovhsqhtka@naver.com>
2023-01-17 14:37:13 +0900

    [chore] : remoteip 및 포트번호 8080 변경

--- src/main/java/shop/itbook/itbookfront/home/HomeController.java
+++ src/main/java/shop/itbook/itbookfront/home/HomeController.java
@@ -1,9 +1,12 @@
 package shop.itbook.itbookfront.home;
 
+import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,17 +21,23 @@ import shop.itbook.itbookshop.category.dto.response.CategoryListResponseDto;
  */
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class HomeController {
 
     private final CategoryService categoryService;
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, HttpServletRequest httpServletRequest) {
         List<CategoryListResponseDto> categoryList = categoryService.findCategoryList("/api/categories");
 
         List<MainCategory> mainCategoryList =
             CategoryUtil.getMainCategoryList(categoryList);
         model.addAttribute("mainCategoryList", mainCategoryList);
+
+
+        String remoteAddr = httpServletRequest.getHeader("X-Forwarded-For");
+        log.info("########## 브라우저 ip : " + remoteAddr);
+
         return "mainpage/index";
     }
 
