6c6b10ef64bc8e328fd7eb1c12925d13df248df1
dahye <sdh46@chosun.kr>
2023-01-25 00:48:10 +0900

    [feat] : elastic 패키지 수정

--- src/main/java/shop/itbook/itbookfront/elastic/controller/serviceapi/ProductSearchController.java
+++ src/main/java/shop/itbook/itbookfront/elastic/controller/serviceapi/ProductSearchController.java
@@ -10,9 +10,12 @@ import shop.itbook.itbookfront.elastic.dto.response.ProductSampleResponseDto;
 import shop.itbook.itbookfront.elastic.service.ProductSearchService;
 
 /**
+ * 상품 검색을 담당하는 컨트롤러입니다.
+ *
  * @author 송다혜
  * @since 1.0
  */
+//TODO : 나중에 일반 컨트롤러로 변환 후 뷰 페이지로 보여주기
 @RestController
 @RequestMapping("/product/search")
 @RequiredArgsConstructor
@@ -20,13 +23,20 @@ public class ProductSearchController {
     private final ProductSearchService productSearchService;
     private static final String DIRECTORY_NAME = "product";
 
+    /**
+     * 이름으로 상품을 검색하여 리스트를 반환하는 메소드입니다.
+     *
+     * @param name 검색할 내용을 받습니다.
+     * @return 상품정보를 반환해줍니다.
+     */
     @GetMapping
-    public List<ProductSampleResponseDto> nameList(@RequestParam String name) {
+    public List<ProductSampleResponseDto> searchProductByName(@RequestParam String name) {
 
-        List<ProductSampleResponseDto> productList = productSearchService.findProductList("/api/products/search?name="+name);
+        List<ProductSampleResponseDto> productList =
+            productSearchService.findProductList("/api/products/search?name="+name);
 
 //        model.addAttribute("productList", productList);
-//        return Strings.concat(DIRECTORY_NAME, "/categoryList");
+//        return Strings.concat(DIRECTORY_NAME, "/productList");
         return productList;
     }
 }
