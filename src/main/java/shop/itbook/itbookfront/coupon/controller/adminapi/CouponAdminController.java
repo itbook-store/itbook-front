e81cbdc80f03762d2a1e65f694bdc5496e6c225f
dahye <sdh46@chosun.kr>
2023-01-27 09:15:38 +0900

    [feat] : 쿠폰 페이지 추가

--- src/main/java/shop/itbook/itbookfront/coupon/controller/adminapi/CouponAdminController.java
+++ src/main/java/shop/itbook/itbookfront/coupon/controller/adminapi/CouponAdminController.java
@@ -1,12 +1,11 @@
 package shop.itbook.itbookfront.coupon.controller.adminapi;
 
-import java.util.List;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -21,7 +20,13 @@ public class CouponAdminController {
     private static final String DIRECTORY_NAME = "couponadmin";
 
     @GetMapping("/coupon-addition")
-    public String couponAddSubCategory(Model model) {
+    public String couponAddPage(Model model){
+
+        return Strings.concat(DIRECTORY_NAME, "/couponAddForm");
+    }
+
+    @PostMapping ("/coupon-addition")
+    public String couponAdd(Model model) {
 
         return Strings.concat(DIRECTORY_NAME, "/couponAddForm");
     }
---
+++ src/main/resources/static/coupon/couponResident.js
@@ -0,0 +1,8 @@
+
+function selectPoint(eventTarget) {
+
+    eventTarget.outerHTML =
+        `<input class="form-control" type="text" placeholder="Default input">
+
+        `;
+}
