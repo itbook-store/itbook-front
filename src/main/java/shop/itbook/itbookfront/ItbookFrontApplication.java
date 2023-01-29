aaa439157bd0cc3d793eeb5e11ac53ed378e5ac4
yihoney <109622@naver.com>
2023-01-29 12:43:53 +0900

    [style] : 관리자 페이지 상품 목록 버튼들 컨벤션 맞춰 수정

--- src/main/java/shop/itbook/itbookfront/ItbookFrontApplication.java
+++ src/main/java/shop/itbook/itbookfront/ItbookFrontApplication.java
@@ -8,14 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ItbookFrontApplication {
-
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     * @author yihoney *
-     * fgfgfgff
-     */
+    
     public static void main(String[] args) {
         SpringApplication.run(ItbookFrontApplication.class, args);
     }
