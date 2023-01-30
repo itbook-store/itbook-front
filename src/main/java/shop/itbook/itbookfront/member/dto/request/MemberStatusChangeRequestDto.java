3c7a2ad0a0422211c022094b2914cab222efdd5c
suy00065 <suy00065@gmail.com>
2023-01-30 15:06:27 +0900

    [refactor] : validation 빠진 부분 추가

--- src/main/java/shop/itbook/itbookfront/member/dto/request/MemberStatusChangeRequestDto.java
+++ src/main/java/shop/itbook/itbookfront/member/dto/request/MemberStatusChangeRequestDto.java
@@ -1,5 +1,6 @@
 package shop.itbook.itbookfront.member.dto.request;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +16,8 @@ import org.hibernate.validator.constraints.Length;
 @ToString
 public class MemberStatusChangeRequestDto {
 
+    @NotBlank(message = "멤버 상태는 null값 및 공백을 허용하지 않습니다.")
+    @Length(max = 255, message = "멤버 상태는 최대 255자까지 허용합니다.")
     private String memberStatusName;
 
     @Length(min=2, max = 255)
