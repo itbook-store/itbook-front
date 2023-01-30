8df459b410757506511d3aa682c1d550b80d3f3f
dahye <sdh46@chosun.kr>
2023-01-30 19:14:08 +0900

    [feat] : 쿠폰페이지 수정

--- src/main/resources/static/coupon/couponResident.js
+++ src/main/resources/static/coupon/couponResident.js
@@ -65,10 +65,14 @@ function selectCheck(name){
     return select.options[select.selectedIndex].value === name;
 }
 
-function selectCoupon(){
-    if(selectCheck("일반쿠폰")){
+function selectCouponDiv(select){
+    // let select = document.getElementById('selectCoupon');
+    // select.options[select.selectedIndex].value === name
+
+    if(select.value === "일반쿠폰"){
         document.getElementById("normalCouponDiv").style.display = 'block';
     }else{
+        document.getElementById("normalCouponDiv").style.display = 'none';
 
     }
 }
@@ -98,4 +102,85 @@ async function checkMemberId() {
         document.getElementById('memberId').value = '';
         document.getElementById('memberIdExists').style.display = 'block';
     }
+}
+
+async function showSubCategory(event, categoryNoList) {
+
+    event.preventDefault();
+
+    let mainCategoryNo = document.getElementById("mainCategory").value;
+
+    let subCategoryCheckBoxDiv = document.getElementById("categoryCheckBox");
+    while (subCategoryCheckBoxDiv.hasChildNodes()) {
+        subCategoryCheckBoxDiv.removeChild(subCategoryCheckBoxDiv.firstChild);
+    }
+    subCategoryCheckBoxDiv.style.display = "block";
+
+    console.log(categoryNoList);
+    await fetch(`/async/${mainCategoryNo}/sub-categories`, {
+        method: "GET"
+    })
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(checkBoxList => {
+                if (checkBoxList.level !== 0) {
+                    let checkBox = document.createElement("input");
+                    let checkBoxText = document.createElement("label");
+                    checkBox.type = "checkbox";
+                    checkBox.name = "categoryNoList";
+                    checkBox.value = checkBoxList.categoryNo;
+                    checkBox.style = "margin-right : 4px";
+                    checkBoxText.style = "margin-right : 7px";
+                    if(categoryNoList && categoryNoList.length !=0)
+                        if (categoryNoList.some(no => no == checkBoxList.categoryNo)) {
+                            checkBox.checked = true;
+                        }
+                    checkBoxText.innerText = checkBoxList.categoryName;
+                    subCategoryCheckBoxDiv.appendChild(checkBox);
+                    subCategoryCheckBoxDiv.appendChild(checkBoxText);
+                }
+            });
+        });
+
+}
+
+
+async function showSearchProductList(name) {
+
+    event.preventDefault();
+
+    let mainCategoryNo = document.getElementById("mainCategory").value;
+
+    let subCategoryCheckBoxDiv = document.getElementById("categoryCheckBox");
+    while (subCategoryCheckBoxDiv.hasChildNodes()) {
+        subCategoryCheckBoxDiv.removeChild(subCategoryCheckBoxDiv.firstChild);
+    }
+    subCategoryCheckBoxDiv.style.display = "block";
+
+    console.log(categoryNoList);
+    await fetch(`/async/${mainCategoryNo}/sub-categories`, {
+        method: "GET"
+    })
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(checkBoxList => {
+                if (checkBoxList.level !== 0) {
+                    let checkBox = document.createElement("input");
+                    let checkBoxText = document.createElement("label");
+                    checkBox.type = "checkbox";
+                    checkBox.name = "categoryNoList";
+                    checkBox.value = checkBoxList.categoryNo;
+                    checkBox.style = "margin-right : 4px";
+                    checkBoxText.style = "margin-right : 7px";
+                    if(categoryNoList && categoryNoList.length !=0)
+                        if (categoryNoList.some(no => no == checkBoxList.categoryNo)) {
+                            checkBox.checked = true;
+                        }
+                    checkBoxText.innerText = checkBoxList.categoryName;
+                    subCategoryCheckBoxDiv.appendChild(checkBox);
+                    subCategoryCheckBoxDiv.appendChild(checkBoxText);
+                }
+            });
+        });
+
 }
