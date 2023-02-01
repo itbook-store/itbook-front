83ed22347a324c5784e0d3d2dd9a477930d4c178
yihoney <109622@naver.com>
2023-02-02 01:51:48 +0900

    [feat] : 상품 유형 메뉴 UI 개발중

--- src/main/resources/static/fragment/adminpage/js/sideMenu.js
+++ src/main/resources/static/fragment/adminpage/js/sideMenu.js
@@ -9,10 +9,24 @@ function selectHowToShowCategories(eventTarget) {
                             data-bs-target="#dashboard-collapse" aria-expanded="false">
                         카테고리 관리
                     </button>
-                    <button style="margin-left: 20%" class="btn btn-toggle" onclick="location.href='/admin/categories'">카테고리 전체 조회</button>
-                    <button style="margin-left: 20%" class="btn btn-toggle mt-1" onclick="location.href='/admin/categories/main-categories'">카테고리 분류별 조회</button>
-                    <button style="margin-left: 20%" class="btn btn-toggle mt-1" onclick="location.href='/admin/category-addition'">카테고리 추가</button>
+                    <button style="margin-left: 10%" class="btn btn-toggle" onclick="location.href='/admin/categories'">카테고리 전체 조회</button>
+                    <button style="margin-left: 10%" class="btn btn-toggle mt-1" onclick="location.href='/admin/categories/main-categories'">카테고리 분류별 조회</button>
+                    <button style="margin-left: 10%" class="btn btn-toggle mt-1" onclick="location.href='/admin/category-addition'">카테고리 추가</button>
         `;
+}
+
+function showProductSubMenu(eventTarget) {
+
+    eventTarget.outerHTML =
+        `                     
+                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                            data-bs-target="#dashboard-collapse" aria-expanded="false">
+                        상품 관리
+                    </button>
+                    <button style="margin-left: 10%" class="btn btn-toggle" onclick="location.href='/admin/products'">상품 목록 조회</button>
+                    <button style="margin-left: 10%" class="btn btn-toggle" onclick="location.href='/admin/products/product-type'">상품 유형 관리</button>
+        `;
+
 }
 
 function selectHowToShowCoupon(eventTarget) {
@@ -23,7 +37,7 @@ function selectHowToShowCoupon(eventTarget) {
                             data-bs-target="#dashboard-collapse" aria-expanded="false">
                         쿠폰 관리
                     </button>
-                    <button class="btn btn-toggle" style="margin-left: 20%" onclick="location.href='/admin/coupon';">쿠폰 전체 조회</a></button>
-                    <button class="btn btn-toggle" style="margin-left: 20%" onclick="location.href='/admin/coupon/coupon-addition';">쿠폰 추가</a></button>
+                    <button class="btn btn-toggle" style="margin-left: 10%" onclick="location.href='/admin/coupon';">쿠폰 전체 조회</a></button>
+                    <button class="btn btn-toggle" style="margin-left: 10%" onclick="location.href='/admin/coupon/coupon-addition';">쿠폰 추가</a></button>
         `;
-}
+}``
