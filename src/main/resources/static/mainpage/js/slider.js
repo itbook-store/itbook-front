dd3ed30587f954879fd0465fd0b547d96cd050a2
yihoney <109622@naver.com>
2023-02-01 18:51:00 +0900

    [feat] : 메인페이지 슬라이더 기능 추가

--- src/main/resources/static/mainpage/js/slider.js
+++ src/main/resources/static/mainpage/js/slider.js
@@ -3,17 +3,12 @@ showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("fade-custom");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000);
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
 }
