87c3125ca88e71c5bcba980fb73259a088d4514b
yihoney <109622@naver.com>
2023-02-02 22:05:49 +0900

    [feat] : 필드명 수정

--- src/main/java/shop/itbook/itbookfront/product/dto/response/ProductDetailsResponseDto.java
+++ src/main/java/shop/itbook/itbookfront/product/dto/response/ProductDetailsResponseDto.java
@@ -24,7 +24,7 @@ public class ProductDetailsResponseDto {
 
     private String detailsDescription;
 
-    private Boolean isExposed;
+    private Boolean isSelled;
 
     private Boolean isForceSoldOut;
 
