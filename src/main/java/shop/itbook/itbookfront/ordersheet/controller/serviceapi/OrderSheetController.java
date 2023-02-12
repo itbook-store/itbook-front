cfae509c5b22ffb541682ffca5bfe7eaded6b08b
HeLLo2 <popt0@naver.com>
2023-02-13 02:15:31 +0900

    [feat] : 구독 주문 진행 중

--- src/main/java/shop/itbook/itbookfront/ordersheet/controller/serviceapi/OrderSheetController.java
+++ src/main/java/shop/itbook/itbookfront/ordersheet/controller/serviceapi/OrderSheetController.java
@@ -21,7 +21,9 @@ import shop.itbook.itbookfront.category.model.MainCategory;
 import shop.itbook.itbookfront.category.service.CategoryService;
 import shop.itbook.itbookfront.category.util.CategoryUtil;
 import shop.itbook.itbookfront.order.dto.request.OrderAddRequestDto;
+import shop.itbook.itbookfront.order.dto.request.OrderSubscriptionRequestDto;
 import shop.itbook.itbookfront.order.dto.response.OrderSheetResponseDto;
+import shop.itbook.itbookfront.order.exception.InvalidOrderException;
 import shop.itbook.itbookfront.ordersheet.service.OrderSheetService;
 
 /**
@@ -74,6 +76,7 @@ public class OrderSheetController {
             orderSheetService.findOrderSheetCartProducts(productNoList, productCntList,
                 memberNo);
 
+
         Queue<Integer> productCntQueue = new LinkedList<>(productCntList);
 
         model.addAttribute("productDetailsList",
@@ -85,9 +88,64 @@ public class OrderSheetController {
         return "mainpage/ordersheet/orderSheetForm";
     }
 
+    /**
+     * 회원의 상품 주문시 해당 상품의 정보와 배송지 정보를 불러옵니다.
+     *
+     * @param productNoList      주문하려는 제품의 번호 리스트
+     * @param productCntList     주문하려는 제품의 개수 리스트
+     * @param orderAddRequestDto 주문하기 위해 주문서에서 작성해야할 값들이 있는 Dto
+     * @return 주문 작성 페이지
+     */
     @PostMapping("/subscription")
-    public String orderSubscriptionSelect(
-        Model model) {
+    @SuppressWarnings("java:S5411")
+    public String orderProductMember(@RequestParam List<Long> productNoList,
+                                     @RequestParam List<Integer> productCntList,
+                                     @RequestParam(value = "subscriptionPeriod")
+                                     Integer subscriptionPeriod,
+                                     @ModelAttribute("orderAddRequestDto")
+                                     OrderAddRequestDto orderAddRequestDto,
+                                     @AuthenticationPrincipal UserDetailsDto userDetailsDto,
+                                     Model model) {
+
+        if (Objects.nonNull(subscriptionPeriod)) {
+            log.info("제발: {}", subscriptionPeriod);
+        }
+
+        // TODO: 2023/02/08 포인트 받아오기
+        // TODO: 2023/02/08 사용가능한 쿠폰 받아오기
+        Optional<Long> memberNo = Optional.empty();
+
+        if (Objects.nonNull(userDetailsDto)) {
+            memberNo = Optional.of(userDetailsDto.getMemberNo());
+        }
+
+        OrderSheetResponseDto orderSheet =
+            orderSheetService.findOrderSheetCartProducts(productNoList, productCntList,
+                memberNo);
+
+        if (!orderSheet.getProductDetailsResponseDtoList().get(0).getIsSubscription()) {
+            throw new InvalidOrderException();
+        }
+
+        Queue<Integer> productCntQueue = new LinkedList<>(productCntList);
+
+        model.addAttribute("productDetailsList",
+            orderSheet.getProductDetailsResponseDtoList());
+        model.addAttribute("productCntQueue", productCntQueue);
+        model.addAttribute("memberDestinationList",
+            orderSheet.getMemberDestinationResponseDtoList());
+        model.addAttribute("subscription", Boolean.TRUE);
+
+        return "mainpage/ordersheet/orderSheetForm";
+    }
+
+    @GetMapping("/subscription")
+    public String orderSubscriptionPeriodSelect(@RequestParam("productNo") Long productNo,
+                                                @RequestParam("productCnt") Integer productCnt,
+                                                Model model) {
+
+        model.addAttribute("productNo", productNo);
+        model.addAttribute("productCnt", productCnt);
 
         return "mainpage/ordersheet/subscriptionOrderForm";
     }
