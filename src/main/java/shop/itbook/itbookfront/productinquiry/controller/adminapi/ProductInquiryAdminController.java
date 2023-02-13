a2f2fcbab456c9c106d5398e7ef8553de191c397
suy00065 <suy00065@gmail.com>
2023-02-14 02:41:53 +0900

    [feat] : 상품문의 구현 중

--- src/main/java/shop/itbook/itbookfront/productinquiry/controller/adminapi/ProductInquiryAdminController.java
+++ src/main/java/shop/itbook/itbookfront/productinquiry/controller/adminapi/ProductInquiryAdminController.java
@@ -1,5 +1,6 @@
 package shop.itbook.itbookfront.productinquiry.controller.adminapi;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shop.itbook.itbookfront.category.dto.response.CategoryListResponseDto;
+import shop.itbook.itbookfront.category.service.CategoryService;
 import shop.itbook.itbookfront.common.response.PageResponse;
 import shop.itbook.itbookfront.productinquiry.dto.response.ProductInquiryCountResponseDto;
 import shop.itbook.itbookfront.productinquiry.dto.response.ProductInquiryResponseDto;
@@ -27,9 +30,20 @@ public class ProductInquiryAdminController {
 
     private final ProductInquiryService productInquiryService;
 
+    private final CategoryService categoryService;
+
+    public static final Integer SIZE_OF_ALL_CONTENT = Integer.MAX_VALUE;
+    public static final Integer PAGE_OF_ALL_CONTENT = 0;
+
     @GetMapping("/list")
     public String productInquiryList(@PageableDefault Pageable pageable,
                                      Model model) {
+
+        List<CategoryListResponseDto> categoryList =
+            categoryService.findCategoryList(String.format("/api/categories?page=%d&size=%d",
+                PAGE_OF_ALL_CONTENT, SIZE_OF_ALL_CONTENT)).getContent();
+
+        model.addAttribute("categoryList", categoryList);
 
         PageResponse<ProductInquiryResponseDto> pageResponse = productInquiryService.findProductInquiryList(
             String.format("?page=%d&size=%d", pageable.getPageNumber(), pageable.getPageSize()));
