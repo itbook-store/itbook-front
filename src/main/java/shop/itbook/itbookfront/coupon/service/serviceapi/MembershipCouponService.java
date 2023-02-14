464d8de2d6720eab97fc1ce19c30a164d1e758f0
dahye <sdh46@chosun.kr>
2023-02-14 12:38:34 +0900

    [feat] : 쿠폰 페이지 생성

---
+++ src/main/java/shop/itbook/itbookfront/coupon/service/serviceapi/MembershipCouponService.java
@@ -0,0 +1,12 @@
+package shop.itbook.itbookfront.coupon.service.serviceapi;
+
+import java.util.List;
+import shop.itbook.itbookfront.coupon.dto.response.MembershipCouponResponseDto;
+
+/**
+ * @author 송다혜
+ * @since 1.0
+ */
+public interface MembershipCouponService {
+    List<MembershipCouponResponseDto> findAvailableMembershipCoupon(String membershipCouponListUrl);
+}
