fe8db1a3a8b6b7bfd3cf9a0372280dc0f997bcff
dahye <sdh46@chosun.kr>
2023-02-18 12:20:01 +0900

    [feat] : 검색 날짜

--- src/main/java/shop/itbook/itbookfront/coupon/controller/adminapi/CouponIssueAdminController.java
+++ src/main/java/shop/itbook/itbookfront/coupon/controller/adminapi/CouponIssueAdminController.java
@@ -1,5 +1,6 @@
 package shop.itbook.itbookfront.coupon.controller.adminapi;
 
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import shop.itbook.itbookfront.common.response.PageResponse;
 import shop.itbook.itbookfront.coupon.dto.response.AdminCouponIssueListResponseDto;
 import shop.itbook.itbookfront.coupon.service.adminapi.CouponIssueAdminService;
+import shop.itbook.itbookfront.member.dto.request.MemberSearchRequestDto;
+import shop.itbook.itbookfront.member.dto.response.MemberAdminResponseDto;
 
 /**
  * @author 송다혜
@@ -25,13 +28,29 @@ public class CouponIssueAdminController {
     private static final String DIRECTORY_NAME = "adminpage/couponadmin";
 
     @GetMapping("/list")
-    public String couponList(Model model, @PageableDefault Pageable pageable){
+    public String couponList(Model model, @PageableDefault Pageable pageable) {
 
         PageResponse<AdminCouponIssueListResponseDto> couponIssueList =
-                couponIssueAdminService.findAllCouponIssueList(
-                        pageable.getPageNumber(), pageable.getPageSize());
+            couponIssueAdminService.findAllCouponIssueList(
+                String.format("/list?page=%d&size=%d", pageable.getPageNumber(),
+                    pageable.getPageSize()));
         model.addAttribute("pageResponse", couponIssueList);
         model.addAttribute("paginationUrl", "/admin/coupon/coupon-issues/list");
+        return Strings.concat(DIRECTORY_NAME, "/couponIssueList");
+    }
+
+    @GetMapping("/search")
+    public String memberSearch(Model model, @PageableDefault Pageable pageable,
+                               String searchTarget, String keyword) {
+
+        PageResponse<AdminCouponIssueListResponseDto> couponIssueList =
+            couponIssueAdminService.findAllCouponIssueList(
+                String.format("/search?page=%d&size=%d&searchTarget=%s&keyword=%s",
+                    pageable.getPageNumber(), pageable.getPageSize(), searchTarget, keyword));
+        model.addAttribute("pageResponse", couponIssueList);
+        model.addAttribute("paginationUrl",
+            String.format("/admin/coupon/coupon-issues/search?searchTarget=%s&keyword=%s",
+                searchTarget, keyword));
         return Strings.concat(DIRECTORY_NAME, "/couponIssueList");
     }
 }
